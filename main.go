eda082ca79e0749b2ef6b3dd7366d63b04925317
Ross Duquette <rduquette@kepler.space>
2025-12-02 17:37:35 -0500

    Extract month and day from user input

--- main.go
+++ main.go
@@ -13,12 +13,17 @@ func is_valid_date(date string) bool {
     return match != ""
 }
 
+func extract_month_day(date string) (month string, day string) {
+    return date[:3], date[4:]
+}
+
 func main() {
     date := flag.String("date", "Jan-01", "Date to solve for")
     flag.Parse()
 
     if is_valid_date(*date) {
-        fmt.Println("Solving a-puzzle-a-day for", *date)
+        month, day := extract_month_day(*date)
+        fmt.Println("Solving a-puzzle-a-day for", month, day)
     } else {
         fmt.Println("Invalid date:", *date)
     }
