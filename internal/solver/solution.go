ca2b24c8330c508e13e6a4419a85abf65d9f473e
Ross Duquette <rduquette@kepler.space>
2025-12-23 16:11:55 -0500

    Save solutions to file when flag is set

--- internal/solver/solution.go
+++ internal/solver/solution.go
@@ -2,18 +2,35 @@ package solver
 
 import (
 	"fmt"
+	"os"
 )
 
 func handle_solutions(solutions <-chan Board, save_to_files bool) {
 	num_solutions := 0
 	for board := range solutions {
-		handle_solved_board(board, save_to_files)
 		num_solutions++
+		handle_solved_board(board, save_to_files, num_solutions)
 	}
 	fmt.Println("Found", num_solutions, "total solutions")
 }
 
-func handle_solved_board(board Board, save_to_files bool) {
-	fmt.Println("Solved!")
-	fmt.Println(board)
+func handle_solved_board(board Board, save_to_files bool, solution_num int) {
+	if save_to_files {
+		// Create dir
+		dir := fmt.Sprintf("solutions/%s-%s", board.solution_month, board.solution_day)
+		os.MkdirAll(dir, 0755)
+
+		// Create file
+		path := fmt.Sprintf("%s/%d.txt", dir, solution_num)
+		file, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		file.WriteString(board.String())
+	} else {
+		fmt.Println("Solved!")
+		fmt.Println(board)
+	}
 }
